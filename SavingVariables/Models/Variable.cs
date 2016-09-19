9fcdb82368f71ee1780fc247fcc10cf1fea59131
Dan Ventura <danwventura@icloud.com>
2016-09-19 17:14:11 -0500

    model classes built/created blank expression_context file

---
+++ SavingVariables/Models/Variable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavingVariables.Models
+{
+    public class Variable
+    {
+        [Key]
+        public int VariableId { get; set; }
+        public string VarName { get; set; }
+        public int VarValue { get; set; }
+
+    }
+
+
+}
---
+++ SavingVariables/VariableListBuilder.cs
@@ -0,0 +1,31 @@
+﻿using SavingVariables.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavingVariables
+{
+    class VariableListBuilder
+    {
+        public virtual List<Variable> VariablesList { get; set; }
+
+        //private int i = 0;
+        public void Add_Variable_To_List(string user_variable_name, int user_variable_value)
+        {
+            Variable new_user_variable = new Variable()
+            {
+                VarName = user_variable_name,
+                VarValue = user_variable_value,
+                //VariableId = i++
+                
+            };
+
+
+            VariablesList.Add(new_user_variable);
+        }
+    }
+
+
+}
